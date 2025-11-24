25f62e75e5889450f883bc9dcb85788997693537
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-11-24 21:01:40 +0900

    Create 02_1.c

---
+++ ch5/02_1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+struct Employee {
+    int emp_id;
+    char name[20];
+    char department[20];
+    double salary;
+};
+
+int main(void) {
+    int fd = open("employee",O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+    if ((fd ==-1)) {
+        perror("faile to open");
+        exit(2);
+    }
+
+
+    struct Employee e_database[4] = {
+        {1001, "Alice", "HR", 3500.50},
+        {1002, "Bob", "Engineering", 5200.75},
+        {1003, "Charlie", "Sales", 4100.00},
+        {1004, "Trudy", "Marketing", 6100.00}
+    };
+
+    lseek(fd, 0L, SEEK_SET);
+    write(fd, &e_database, 4 * sizeof(struct Employee));
+
+    close(fd);
+    printf("Employee data written successfully.\n");
+    return 0;
+}
