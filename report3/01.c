bada29a1ebffcfad3d63f3cf69d2647b1f1bc0e5
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-12-19 15:57:11 +0900

    Update 01.c

--- report3/01.c
+++ report3/01.c
@@ -1,67 +1,54 @@
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-struct student {
-    int   id;
-    int   grade;        
-    char  name[20];
-    float gpa;          
-};
+int main() {
+    int number;
+    printf("Input the number of child processes: ");
+    scanf("%d", &number);
 
-int main(void)
-{
-    FILE *fp;
-    struct student *arr = NULL;
-    int capacity = 10;      
-    int count = 0;          
-
-    fp = fopen("students.txt", "r");
-    if (fp == NULL) {
-        perror("students.txt");
+    if (number <= 0) {
+        printf("Wrong input. Input a number greater than 0\n");
         exit(0);
     }
 
-    arr = (struct student *)malloc(sizeof(struct student) * capacity);
-    if (arr == NULL) {
+    int *pid = (int *)malloc(sizeof(int) * number);
+    if (pid == NULL) {
         perror("malloc");
-        fclose(fp);
-        exit(0);
+        exit(1);
     }
 
+    for (int i = 0; i < number; i++) {
+        pid[i] = fork();
 
-    while (1) {
-        struct student tmp;
-        int n = fscanf(fp, "%d %d %19s %f",
-                       &tmp.id, &tmp.grade, tmp.name, &tmp.gpa);
-        if (n != 4)      
-            break;
-
-        
-        if (count == capacity) {
-            capacity += 10;
-            struct student *new_arr =
-                (struct student *)realloc(arr, sizeof(struct student) * capacity);
-            if (new_arr == NULL) {
-                perror("realloc");
-                free(arr);
-                fclose(fp);
-                return 1;
-            }
-            arr = new_arr;
+        if (pid[i] < 0) {
+            perror("fork");
+            exit(1);
         }
 
-        arr[count++] = tmp;
+        if (pid[i] == 0) {
+            
+            printf("I am %dth child. PID = %d, Parent PID = %d\n",
+                   i + 1, getpid(), getppid());
+            exit(0);   
+        }
+        
     }
 
-    fclose(fp);
+    
+    while (wait(NULL) > 0);
 
-    printf("-----------Student Information-----------\n");
-    for (int i = 0; i < count; i++) {
-        printf("ID: %4d, Grade: %d, Name: %-10s, GPA: %.2f\n",
-               arr[i].id, arr[i].grade, arr[i].name, arr[i].gpa);
+    printf("[Parent %d] All children have terminated\n", getpid());
+
+    
+    printf("Childs Pid : ");
+    for (int i = 0; i < number; i++) {
+        printf("%d ", pid[i]);
     }
-    printf("Total %d students.\n", count);
+    printf("\n");
 
-    free(arr);
+    free(pid);
     return 0;
 }
