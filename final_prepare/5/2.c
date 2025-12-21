f4ef527f12614c01554cfcf7977d7c9ac38e443e
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-12-21 10:22:54 +0900

    Create 2.c

---
+++ final_prepare/5/2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>   
+#include <fcntl.h>    
+
+struct Employee {
+    int emp_id;
+    char name[20];
+    char department[20];
+    double salary;
+};
+
+int main(void) {
+    int fd = open("employee",O_RDONLY);
+    if (fd == -1) {
+        perror("Fail to open employee file");
+        exit(1);
+    }
+
+    struct Employee emp;
+    printf("=== Employee Records ===\n");
+
+
+    while (read(fd, &emp, sizeof(struct Employee)) > 0) {
+        printf("ID: %4d | Name: %-8s | Department: %-11s | Salary: %.2lf USD\n",
+               emp.emp_id, emp.name, emp.department, emp.salary);
+    }
+
+    close(fd);
+    return 0;
+}
