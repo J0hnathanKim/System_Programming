4602afd619c9d0495de8161c175727ad18fc5f46
Johnathan Kim <99706992+J0hnathanKim@users.noreply.github.com>
2025-12-21 19:31:29 +0900

    Update 12.c

--- final_prepare/12.c
+++ final_prepare/12.c
@@ -69,7 +69,7 @@ int main(int argc, char *argv[]) {
         int n = read(fd_r, buf, MAXLINE);
         if (n > 0){
             printf("[Received] %s", buf);
-            fflush(stdout); //필수 X
+            
         }
     }
 
